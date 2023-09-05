e93fb76f49939781b0f42dacb2482732224ffbc9
ARProxy <tnqlsdld1@naver.com>
2023-09-05 15:41:47 +0900

    cote1

---
+++ cote1.java
@@ -0,0 +1,20 @@
+public class cote1 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        int result = test.Solution(1234);
+        System.out.println("result : " + result);
+    }
+}
+class Test {
+    public int Solution(int n) {
+        int answer = 0;
+        String s = Integer.toString(n);
+
+        for(int i=0; i<s.length(); i++) {
+            answer += Integer.parseInt(s.substring(i, i+1));
+        }
+
+        return answer;
+    }
+    
+}
